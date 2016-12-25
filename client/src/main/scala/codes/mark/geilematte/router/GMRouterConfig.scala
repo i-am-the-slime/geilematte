2088643d2e6e9a00e0ee8c6ef0bd6da904c269ff
Mark Eibes <mark.eibes@zalando.de>
2016-12-25 23:05:05 +0100

    compiling once more

--- client/src/main/scala/codes/mark/geilematte/router/GMRouterConfig.scala
+++ client/src/main/scala/codes/mark/geilematte/router/GMRouterConfig.scala
@@ -2,25 +2,25 @@ package codes.mark.geilematte.router
 
 import codes.mark.geilematte.components.GameMenu
 import codes.mark.geilematte.html.Static
+import codes.mark.geilematte.remote.LoginHandler
 import codes.mark.geilematte.{Login, Register, SessionInfo, UserId}
-import japgolly.scalajs.react.extra.router.{
-  Redirect,
-  Resolution,
-  RouterConfigDsl,
-  RouterCtl
-}
+import japgolly.scalajs.react.extra.router.{Redirect, Resolution, RouterConfigDsl, RouterCtl}
 import japgolly.scalajs.react.Callback
+import japgolly.scalajs.react.extra.router.StaticDsl.RouteB
 import org.scalajs.dom
 
 sealed trait GMPage
 case object LoginPage                                       extends GMPage
 case object RegisterPage                                    extends GMPage
 case object NotFoundPage                                    extends GMPage
-final case class GameMenuPage(userId: Int, session: String) extends GMPage
+final case class GameMenuPage(userId: UserId, session: SessionInfo) extends GMPage
 case object EditorPage                                      extends GMPage
 //final case class Encounter(userId: Int, session: String)    extends GMPages
 
 object GMPage {
+  val userIdParam  = new RouteB[UserId] ("(-?\\d+)", 1, g => Some(UserId(g(0).toInt)), _.toString)
+  val sessionParam = new RouteB[SessionInfo] ("(-?[a-zA-Z0-9]+)", 1, g => Some(SessionInfo(g(0))), _.toString)
+
   val routerConfig = RouterConfigDsl[GMPage].buildConfig { dsl =>
     import dsl._
 
@@ -30,8 +30,9 @@ object GMPage {
       | staticRoute("login", LoginPage) ~> renderR(
         route =>
           Login.component(
+//            GameMenuPage.apply _ andThen route.set _
             (id: UserId, info: SessionInfo) =>
-              route.set(GameMenuPage(id.id, info.unboxed))
+              route.set(GameMenuPage(id, info))
         )
       )
       | staticRoute("register", RegisterPage) ~> renderR(
@@ -41,12 +42,11 @@ object GMPage {
         Static.notFound
       )
       | dynamicRouteCT(
-        ("menu" / int / string("[a-zA-Z0-9]+"))
-          .caseClass[GameMenuPage]
+        ("menu" / userIdParam / sessionParam).caseClass[GameMenuPage]
       ) ~>
         dynRender(
           (gmp: GameMenuPage) => {
-            GameMenu.component((UserId(gmp.userId), SessionInfo(gmp.session)))
+            GameMenu.component((gmp.userId, gmp.session))
           }
         ))
       .notFound(redirectToPage(NotFoundPage)(Redirect.Replace))
