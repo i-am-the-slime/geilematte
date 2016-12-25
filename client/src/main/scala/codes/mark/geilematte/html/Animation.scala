b084cee05a5ee595548c84208e2dc6b1c3ff1fad
Mark Eibes <mark.eibes@zalando.de>
2016-12-25 12:35:53 +0100

    Progress.

---
+++ client/src/main/scala/codes/mark/geilematte/html/Animation.scala
@@ -0,0 +1,8 @@
+package codes.mark.geilematte.html
+
+/**
+  * Created by meibes on 24.12.16.
+  */
+object Animation {
+
+}
---
+++ server/src/main/scala/codes/mark/geilematte/logging/Logging.scala
@@ -0,0 +1,8 @@
+package codes.mark.geilematte.logging
+
+/**
+  * Created by meibes on 24.12.16.
+  */
+class Logging {
+
+}
